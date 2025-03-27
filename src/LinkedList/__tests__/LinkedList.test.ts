c753cd3ad17278f38ad98d792f1e520f5774462e
Aaron Young <luozhu2021@gmail.com>
2025-03-27 08:23:15 +0800

    chore: 编写测试用例

--- src/LinkedList/__tests__/LinkedList.test.ts
+++ src/LinkedList/__tests__/LinkedList.test.ts
@@ -53,10 +53,34 @@ describe('LinkedList', () => {
     expect(linkedList.size()).toBe(3);
   });
 
+  it('should remove the first element', () => {
+    linkedList.push(1);
+    linkedList.push(2);
+    linkedList.push(3);
+    expect(linkedList.removeAt(0)).toBe(1);
+  });
+
   it('should remove the last element', () => {
     linkedList.push(1);
     linkedList.push(2);
     linkedList.push(3);
     expect(linkedList.removeAt(2)).toBe(3);
+  });
+
+  it('should insert element at the beginning of the list', () => {
+    linkedList.insert(1, 0);
+    expect(linkedList.head?.element).toBe(1);
+    expect(linkedList.count).toBe(1);
+  });
+
+  it('should insert element at the end of the list', () => {
+    linkedList.insert(1, 0);
+    linkedList.insert(2, 1);
+    expect(linkedList.getElementAt(1)?.element).toBe(2);
+    expect(linkedList.count).toBe(2);
+  });
+
+  it('should return false if out of bounds or an exception is thrown', () => {
+    expect(linkedList.insert(1, -1)).toBeFalsy();
   });
 });
